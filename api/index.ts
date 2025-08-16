103d99acd49397f678a75425fff81b4b922cc5cf
Sagar Nanji <you@example.com>
2025-08-16 04:01:14 -0400

    file changed

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,5 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src/server";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  // Express apps are valid Node handlers (req, res)
   return (app as any)(req, res);
 }
